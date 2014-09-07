f8ea02974e3f0333e11776563b09b8aaee0b2167
to-fer <tony.j.ferrero@gmail.com>
2014-09-07 16:37:14 -0700

    Fix naming conflict.

--- CS 156 Homework 1/CS 156 Homework 1/BoardSquare.py
+++ CS 156 Homework 1/CS 156 Homework 1/BoardSquare.py
@@ -4,5 +4,6 @@ __author__ = "Christopher Raleigh"
 class BoardSquare(object):
     """Represents an object in a space of a board."""
 
-    def __init__(self, type):
-        self.type = type
+    # Using a strange name here because "type" is a built-in identifier
+    def __init__(self, typ):
+        self.type = typ
