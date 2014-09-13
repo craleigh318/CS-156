eeae05b54b3961d03aa2214dbf7f0c08b0bcc8ae
to-fer <tony.j.ferrero@gmail.com>
2014-09-12 22:23:25 -0700

    Naming fix.

--- CS 156 Homework 1/CS 156 Homework 1/action.py
+++ CS 156 Homework 1/CS 156 Homework 1/action.py
@@ -3,10 +3,11 @@ __author__ = 'Anthony Ferrero'
 
 class Action(object):
     """Represents a singly-linked list of actions."""
+
     def __init__(self, parent=None):
         self._parent = parent
 
-    def parent(self):
+    def get_parent(self):
         return self._parent
 
 
