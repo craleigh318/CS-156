ac784af1021c31c00a985ee381e4fdfaaff6b309
to-fer <tony.j.ferrero@gmail.com>
2014-09-13 03:05:52 -0700

    Finalize node design. (keep track of agent location in node rather than actions; it's simpler to print the board this way).

--- CS 156 Homework 1/CS 156 Homework 1/action.py
+++
@@ -1,8 +0,0 @@
-__author__ = "Christopher Raleigh and Anthony Ferrero"
-
-
-class Action(object):
-    up = 0
-    down = 1
-    right = 2
-    left = 3
--- CS 156 Homework 1/CS 156 Homework 1/node.py
+++ CS 156 Homework 1/CS 156 Homework 1/node.py
@@ -2,11 +2,12 @@ __author__ = "Christopher Raleigh and Anthony Ferrero"
 
 
 class Node(object):
-    """Represents a singly-linked list of nodes."""
+    """Represents a tree of nodes, used for searching. Note: we don't need to explicitly keep track of the action used
+    to generate each node; tracking the location of the agent when each node is explored is enough."""
 
-    def __init__(self, cost, action=None, parent=None):
+    def __init__(self, agent_location, cost, parent=None):
+        self._agent_location = agent_location
         self._cost = cost
-        self._action = action
         self._parent = parent
 
     def get_cost(self):
