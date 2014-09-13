823214a8f03ff879b9d822ca9eb3d7fafe61ad9b
to-fer <tony.j.ferrero@gmail.com>
2014-09-12 22:16:27 -0700

    Add Action linked list class.

---
+++ CS 156 Homework 1/CS 156 Homework 1/action.py
@@ -0,0 +1,30 @@
+__author__ = 'Anthony Ferrero'
+
+
+class Action(object):
+    """Represents a singly-linked list of actions."""
+    def __init__(self, parent):
+        self._parent = parent
+
+    def parent(self):
+        return self._parent
+
+
+class Up(Action):
+    def __init__(self, parent):
+        super.__init(parent)
+
+
+class Down(Action):
+    def __init__(self, parent):
+        super.__init(parent)
+
+
+class Right(Action):
+    def __init__(self, parent):
+        super.__init(parent)
+
+
+class Left(Action):
+    def __init__(self, parent):
+        super.__init(parent)
