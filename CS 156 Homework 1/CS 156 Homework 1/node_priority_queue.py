ecc701731aae0945d2b1b796ca4bcea7d0c06187
to-fer <tony.j.ferrero@gmail.com>
2014-09-13 21:40:06 -0700

    Make priority queue only store agent location rather than entire nodes (to keep things the same as in the book).

--- CS 156 Homework 1/CS 156 Homework 1/node_priority_queue.py
+++ CS 156 Homework 1/CS 156 Homework 1/node_priority_queue.py
@@ -5,15 +5,15 @@ import heapq
 
 class NodePriorityQueue(object):
 
-    def __init__(self, start_node):
+    def __init__(self, start_agent_location):
         self._internal_list = []
-        self.push(start_node)
+        self.push(start_agent_location)
 
     def __len__(self):
         return len(self._internal_list)
 
-    def push(self, node):
-        heapq.heappush(self._internal_list, (node.get_cost(), node))
+    def push(self, agent_location, cost):
+        heapq.heappush(self._internal_list, (cost, agent_location))
 
     def pop(self):
         return heapq.heappop(self._internal_list)
