798abdc5e739de22761095012f151a6b11d1380a
Christopher Raleigh <craleigh318@comcast.net>
2014-09-14 17:30:12 -0700

    Started solution(tree).

--- CS 156 Homework 1/CS 156 Homework 1/food_agent_ai.py
+++ CS 156 Homework 1/CS 156 Homework 1/food_agent_ai.py
@@ -10,12 +10,18 @@ class FoodAgentAI(object):
         self.board_state = board_state
         self._heuristic = heuristic
         self.board_is_unsolvable = False
-        self.solution_list = [Direction.down, Direction.right, Direction.right]
+        self.solution_list = []
 
     def on_food_agent_turn(self):
         """Actions for the AI to perform on its agent's turn."""
         direction = self.recommend_direction()
         self.board_state.agent.move(direction)
+
+    def solution(self, tree):
+        """Returns the path that the agent should take in the form of a list."""
+        self.solution_list.append(Direction.down)
+        self.solution_list.append(Direction.right)
+        self.solution_list.append(Direction.right)
 
     def recommend_direction(self):
         """Returns a direction that will lead to a solution."""
