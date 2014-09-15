d20a0994db197fba03e560d7a868f98505d0cfe1
to-fer <tony.j.ferrero@gmail.com>
2014-09-14 19:15:19 -0700

    Keep track of agent start location so it can be reset.

--- CS 156 Homework 1/CS 156 Homework 1/board_state.py
+++ CS 156 Homework 1/CS 156 Homework 1/board_state.py
@@ -2,6 +2,7 @@ __author__ = "Christopher Raleigh and Anthony Ferrero"
 
 from food_agent import FoodAgent
 from board_square_type import BoardSquareType
+from board_square import BoardSquare
 
 
 class BoardState(object):
@@ -10,9 +11,13 @@ class BoardState(object):
     def __init__(self, board, x, y):
         self.board = board
         self.agent = FoodAgent(board, x, y)
+        self.agent_start_location = BoardSquare(x, y)
 
     def food_eaten(self):
         """Returns true if the food agent occupies a space with food."""
         agent = self.agent
         return self.board.squares[agent.x][agent.y] == BoardSquareType.food
+
+    def reset_agent_position(self):
+        self.agent.set_location(self.agent_start_location)
 
