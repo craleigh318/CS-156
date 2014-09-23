1bced291b7c9e2d2256a18973604834097872336
Christopher Raleigh <craleigh318@comcast.net>
2014-09-23 02:25:42 -0700

    Added Deck and Suite classes.

--- Homework 2/crazy_eights.py
+++ Homework 2/crazy_eights.py
@@ -13,6 +13,91 @@ class CrazyEight(object):
         pass
 
 
+class Suite(object):
+    """A collection of 13 cards."""
+
+    @staticmethod
+    def num_cards():
+        return 13
+
+    @staticmethod
+    def ace():
+        return 0
+
+    @staticmethod
+    def two():
+        return 1
+
+    @staticmethod
+    def three():
+        return 2
+
+    @staticmethod
+    def four():
+        return 3
+
+    @staticmethod
+    def five():
+        return 4
+
+    @staticmethod
+    def six():
+        return 5
+
+    @staticmethod
+    def seven():
+        return 6
+
+    @staticmethod
+    def eight():
+        return 7
+
+    @staticmethod
+    def nine():
+        return 8
+
+    @staticmethod
+    def ten():
+        return 9
+
+    @staticmethod
+    def jack():
+        return 10
+
+    @staticmethod
+    def queen():
+        return 11
+
+    @staticmethod
+    def king():
+        return 12
+
+
+class Deck(object):
+    """The collection of all 52 cards."""
+
+    @staticmethod
+    def spades():
+        return 0
+
+    @staticmethod
+    def hearts():
+        return 1
+
+    @staticmethod
+    def diamonds():
+        return 2
+
+    @staticmethod
+    def clubs():
+        return 3
+
+    def get_card(self, card, suit):
+        ret = suit * Suite.num_cards()
+        ret += card
+        return ret
+
+
 class Player(object):
     """An actor in the game."""
     pass
