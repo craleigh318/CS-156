d3857131c3cd35ad689db8874065fea85fbc1295
Tony Ferrero <tony.j.ferrero@gmail.com>
2014-10-03 20:55:58 -0700

    Move game-related code and code involving the human player to driver.py (the assignment specifies that this stuff is supposed to be there).

--- Homework 2/crazy_eights.py
+++ Homework 2/crazy_eights.py
@@ -2,8 +2,6 @@ __author__ = 'Christopher Raleigh and Anthony Ferrero'
 
 import random
 
-import driver
-
 
 class CrazyEight(object):
     """Contains methods for AI actions."""
@@ -388,65 +386,3 @@ class Move(object):
         else:
             value = False
         return value
-
-
-class HumanPlayer(object):
-    @staticmethod
-    def move(partial_state):
-        return driver.move(partial_state)
-
-    @staticmethod
-    def move_perfect_knowledge(state):
-        return driver.move(state.partial_state)
-
-
-class AIPlayer(object):
-    @staticmethod
-    def move(partial_state):
-        return CrazyEight.move(partial_state)
-
-    @staticmethod
-    def move_perfect_knowledge(state):
-        return CrazyEight.move_perfect_knowledge(state)
-
-
-def perform_move(state, move):
-    # Draw cards.
-    for i in xrange(0, move.number_of_cards):
-        drawn_card = state.deck.draw_card()
-        state.partial_state.hand.add_card(drawn_card)
-    # Move face-up card.
-    state.partial_state.hand.remove_card(move.face_up_card)
-    state.partial_state.face_up_card = move.face_up_card
-    # Set suit.
-    state.partial_state.suit = move.suit
-    # Add this move to history.
-    state.next_turn(move)
-
-
-def game_loop(state, current_player, next_player):
-    """Cycles through player turns until the game is over."""
-    player_move = current_player.move(state.partial_state)
-    perform_move(state, player_move)
-    # Loop while game is not over.  Switch players.
-    if not state.game_ended():
-        game_loop(state, next_player, current_player)
-
-
-def start_game():
-    """Initializes game objects."""
-    human_choice = driver.get_first_player()
-    if human_choice == 1:
-        player_1 = HumanPlayer
-        player_2 = AIPlayer
-    elif human_choice == 2:
-        player_1 = AIPlayer
-        player_2 = HumanPlayer
-    else:
-        return
-    current_state = State()
-    game_loop(current_state, player_1, player_2)
-
-
-if __name__ == '__main__':
-    start_game()
--- Homework 2/driver.py
+++ Homework 2/driver.py
@@ -1,5 +1,63 @@
 __author__ = 'Christopher Raleigh and Anthony Ferrero'
 
+"""
+Acts as the main module and contains all code related to simulating the game of Crazy Eights and interacting with
+both the human and AI players.
+"""
+
+from crazy_eights import *
+
+
+class HumanPlayer(object):
+    @staticmethod
+    def move(partial_state):
+        return HumanPlayer.move(partial_state)
+
+    @staticmethod
+    def move_perfect_knowledge(state):
+        return HumanPlayer.move(state.partial_state)
+
+    @staticmethod
+    def move(partial_state):
+        """Gets a tuple from the player's input."""
+        print("Enter input as a tuple:")
+        str_input = raw_input()
+        tpl_input = tuple(str_input)
+        return tpl_input
+
+
+class AIPlayer(object):
+    @staticmethod
+    def move(partial_state):
+        return CrazyEight.move(partial_state)
+
+    @staticmethod
+    def move_perfect_knowledge(state):
+        return CrazyEight.move_perfect_knowledge(state)
+
+
+def perform_move(state, move):
+    # Draw cards.
+    for i in xrange(0, move.number_of_cards):
+        drawn_card = state.deck.draw_card()
+        state.partial_state.hand.add_card(drawn_card)
+    # Move face-up card.
+    state.partial_state.hand.remove_card(move.face_up_card)
+    state.partial_state.face_up_card = move.face_up_card
+    # Set suit.
+    state.partial_state.suit = move.suit
+    # Add this move to history.
+    state.next_turn(move)
+
+
+def game_loop(state, current_player, next_player):
+    """Cycles through player turns until the game is over."""
+    player_move = current_player.move(state.partial_state)
+    perform_move(state, player_move)
+    # Loop while game is not over.  Switch players.
+    if not state.game_ended():
+        game_loop(state, next_player, current_player)
+
 
 def get_first_player():
     """Gets the first player. 0 is the human.  1 is the AI."""
@@ -10,9 +68,20 @@ def get_first_player():
     return int_input
 
 
-def move(partial_state):
-    """Gets a tuple from the player's input."""
-    print("Enter input as a tuple:")
-    str_input = raw_input()
-    tpl_input = tuple(str_input)
-    return tpl_input
+def start_game():
+    """Initializes game objects."""
+    human_choice = get_first_player()
+    if human_choice == 1:
+        player_1 = HumanPlayer
+        player_2 = AIPlayer
+    elif human_choice == 2:
+        player_1 = AIPlayer
+        player_2 = HumanPlayer
+    else:
+        return
+    current_state = State()
+    game_loop(current_state, player_1, player_2)
+
+
+if __name__ == '__main__':
+    start_game()
