81074380f8ff64cb6a8469b50527fa8fe7b5f8a4
Tony Ferrero <tony.j.ferrero@gmail.com>
2014-10-05 21:35:34 -0700

    Add legal_moves test.

--- Homework 2/crazy_eights_test.py
+++ Homework 2/crazy_eights_test.py
@@ -11,7 +11,8 @@ class MyTestCase(unittest.TestCase):
 
 
 class StateTest(unittest.TestCase):
-    def test_best_move(self):
+    @staticmethod
+    def make_test_state():
         ai_hand = crazy_eights.Hand([crazy_eights.Card(x) for x in [0, 1, 2, 3, 4, 5, 6, 7]])
         human_hand = crazy_eights.Hand([crazy_eights.Card(x) for x in [8, 9, 10, 11, 12, 13, 14]])
         face_up_card = crazy_eights.Card(16)
@@ -21,14 +22,31 @@ class StateTest(unittest.TestCase):
             face_up_card=face_up_card,
             suit=face_up_card.suit,
             hand=human_hand,
-            history=[crazy_eights.Move.play(0, 2, 1)]
+            history=[crazy_eights.Move.play(0, face_up_card.rank, face_up_card.suit)]
         )
         state = crazy_eights.State(
             hand=ai_hand,
             deck=crazy_eights.Deck(deck_cards),
             partial_state=partial_state
         )
-        depth_limit = 4
+        return state
+
+    def test_legal_moves(self):
+        state = StateTest.make_test_state()
+        expected = [crazy_eights.Move.play(1, 3, 0),
+                    crazy_eights.Move.play(1, 7, 0),
+                    crazy_eights.Move.play(1, 7, 1),
+                    crazy_eights.Move.play(1, 7, 2),
+                    crazy_eights.Move.play(1, 7, 3),
+                    crazy_eights.Move.draw(1, 1)]
+        actual = state._State__legal_moves(state.hand)
+        expected = sorted([m.to_tuple() for m in expected])
+        actual = sorted([m.to_tuple() for m in actual])
+        self.assertEqual(expected, actual)
+
+    def test_best_move(self):
+        state = StateTest.make_test_state()
+        depth_limit = 8
         expected = (1, 3, 0, 0)
         self.assertEqual(expected, state.best_move(depth_limit).to_tuple())
 
