e9131cbf1e164e06b3210eb2a17ae6efc71c8ac0
Christopher Raleigh <craleigh318@comcast.net>
2014-12-07 02:43:49 -0800

    Expanded some classes.

--- Homework 5/connect_learner.py
+++ Homework 5/connect_learner.py
@@ -1,12 +1,34 @@
 import sys
 
 import abstract_classes
-from algorithms import DecisionTree
 
 
 __author__ = 'Christopher Raleigh and Anthony Ferrero'
 
-abstract_classes.Tree.register(DecisionTree)
+
+class Grid(abstract_classes.Example):
+    def __init__(self):
+        self.__matrix = [[0 for i in range(Grid.__get_grid_length())]
+                         for j in range(Grid.__get_grid_length())]
+
+    @staticmethod
+    def __get_grid_length():
+        return 5
+
+    @staticmethod
+    def from_string_collection(collection):
+        new_grid = Grid()
+        # Assign each number to the grid.
+
+
+def file_to_string_collection(file_name):
+    string_list = []
+    with open(file_name) as file:
+        file_lines = file.readlines()
+        for line in file_lines:
+            string_list.append(line)
+    string_tuple = tuple(string_list)
+    return string_tuple
 
 
 def train(training_file_name):
